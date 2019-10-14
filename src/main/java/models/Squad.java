5c62c0d7d340929243aed4293f59ecd0c49d6db0
melissa kobia <melissakobia1997@gmail.com>
2019-10-14 04:37:10 +0300

    Adds a test that checks whether squad intstiates correctly with name and cause

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -5,7 +5,22 @@ public class Squad {
     private String cause;
     private int maxSize;
 
-    public Squad() {
+    public Squad(String name, String cause, int maxSize) {
+        this.name = name;
+        this.cause = cause;
+        this.maxSize = maxSize;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
     }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -9,8 +9,20 @@ import static org.junit.Assert.assertTrue;
 public class SquadTest {
     @Test
     public void SquadIntantiatesCorrectly_true() throws Exception {
-        Squad mysquad = new Squad();
-        assertTrue(mysquad instanceof Squad);
+        Squad mysquad = new Squad("Peace Makers", "Peace",3 );
+        assertTrue(true);
 
+    }
+
+    @Test
+    public void SquadInstantiatesWithName_true() throws Exception {
+        Squad mySquad = new Squad("Peace Makers", "Peace",3 );
+        assertEquals("Peace Makers", mySquad.getName());
+    }
+
+    @Test
+    public void mySquadGetsCause() throws Exception {
+        Squad mySquad = new Squad("Peace Makers", "Peace",3 );
+        assertEquals( "Peace", mySquad.getCause());
     }
 }
